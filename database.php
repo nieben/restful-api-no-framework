7fce5e3c7a5ebb179c17ccd94b39134fd49b4455
Nie Ben <nieben201@hotmail.com>
2016-12-15 09:25:40 +0800

    Update database.php

--- database.php
+++ database.php
@@ -10,6 +10,6 @@ $mysql_server_name='localhost'; //改成自己的mysql数据库服务器
 
 $mysql_username='root'; //改成自己的mysql数据库用户名
 
-$mysql_password='ciefr'; //改成自己的mysql数据库密码
+$mysql_password=''; //改成自己的mysql数据库密码
 
-$mysql_database='ec_survey'; //改成自己的mysql数据库名
+$mysql_database=''; //改成自己的mysql数据库名
